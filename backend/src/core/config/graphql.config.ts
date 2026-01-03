161778ccfd95f81feac06bb790c531b942422ae1
Nikita Morkovkin <nikitamorkovkinwork@gmail.com>
2026-01-03 15:31:19 +0500

    🐳 chore(backend):
    - Initialized GraphQL
    - Created prisma service

---
+++ backend/src/core/config/graphql.config.ts
@@ -0,0 +1,15 @@
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigService } from '@nestjs/config';
+import type { Request, Response } from 'express';
+import { join } from 'path';
+import { isDev } from '../shared/utils/is-dev.util';
+
+export const getGraphQLConfig = (
+  configService: ConfigService,
+): ApolloDriverConfig => ({
+  playground: isDev(configService),
+  path: configService.getOrThrow<string>('GRAPHQL_PATH'),
+  autoSchemaFile: join(process.cwd(), 'src/core/graphql/schema.gql'),
+  sortSchema: true,
+  context: ({ req, res }: { req: Request; res: Response }) => ({ req, res }),
+});
---
+++ backend/src/core/core.module.ts
@@ -0,0 +1,27 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { getGraphQLConfig } from './config/graphql.config';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        ...getGraphQLConfig(configService),
+      }),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class CoreModule {}
